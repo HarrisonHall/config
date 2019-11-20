a0d283ce89236671028d27a8d31d319bc739722f
harrisonhall <harrisonchristianhall@gmail.com>
2019-11-19 19:26:31 -0500

    setup and emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,99 @@
+;; load emacs 24's package system. Add MELPA repository.
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+;; melpa
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default))
+ '(package-selected-packages
+   '(darkroom elpy flycheck flycheck-pycheckers markdown-mode+ markdown-preview-mode dracula-theme markdown-mode auto-complete rainbow-delimiters)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(linum ((t (:slant normal))))
+ )
+
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+(setq auto-revert-mode t)
+(setq scroll-step 1
+      scroll-conservatively 10000)
+
+(setq make-backup-files nil)
+(setq-default indent-tabs-mode nil)
+(xterm-mouse-mode 1)
+(electric-pair-mode 1)
+(column-number-mode)
+(setq warning-minimum-level :emergency) 
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(require 'linum)
+(global-linum-mode t)
+
+;; Wrap lines at 80 characters
+(add-hook 'prog-mode-hook 'auto-fill-mode)
+;; (setq-default fill-column 80)
+
+;; python
+(elpy-enable) 
+
+;; custom keys
+(global-set-key (kbd "C-x |") 'split-window-right)
+(global-set-key (kbd "C-x \\") 'split-window-right)
+(global-set-key (kbd "C-x -") 'split-window-below)
+(global-set-key (kbd "C-x _") 'split-window-below)
+(global-set-key (kbd "C-x x") 'delete-window)
+(global-set-key (kbd "C-x <right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "C-x <up>") 'enlarge-window-vertically)
+(global-set-key (kbd "C-x <left>") 'shrink-window-horizontally)
+(global-set-key (kbd "C-x <down>") 'shrink-window-horizontally)
+(global-set-key (kbd "C-x r") 'query-replace)
+(global-set-key (kbd "C-x q") 'keyboard-quit)
+(global-set-key (kbd "C-u") 'undo)
+
+
+;; use customized linum-format: add a addition space after the line number
+(setq linum-format (lambda (line) (propertize (format (let ((w (length (number-to-string (count-lines (point-min) (point-max)))))) (concat "%" 
+(number-to-string w) "d ")) line) 'face 'linum)))
+
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+(load-theme 'dracula)
+
+(defun on-after-init ()
+  (unless (display-graphic-p (selected-frame))
+    (set-face-background 'default "unspecified-bg"
+  (selected-frame))))
+(add-hook 'window-setup-hook 'on-after-init)
---
+++ setup.sh
@@ -0,0 +1,25 @@
+alias ins="sudo apt install"
+
+
+# General
+ins cmus  # Terminal music player
+ins ranger  # Terminal file browser
+ins tmux  # Terminal multiplexer
+ins fonts-hack-ttf  # Hack font
+ins emacs  # Duh...
+ins htop  # Top
+ins vtop  # Top
+ins gotop-cjbassi  # Nice top
+
+# Icons
+sudo add-apt-repository ppa:papirus/papirus
+sudo apt-get update
+sudo apt-get install papirus-icon-theme
+
+# Ubuntu
+ins unity-tweak-tool
+
+# Manually setup/install:
+# arc dark
+# Papirus fonts
+
