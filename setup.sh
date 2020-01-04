542d0ccbf44e87c1c0145a48c364031297e4e59d
harrisonhall <harrisonchristianhall@gmail.com>
2020-01-04 15:34:15 -0500

    qterminal and hack font

--- setup.sh
+++ setup.sh
@@ -5,11 +5,12 @@ alias ins="sudo apt install"
 ins cmus  # Terminal music player
 ins ranger  # Terminal file browser
 ins tmux  # Terminal multiplexer
-ins fonts-hack-ttf  # Hack font
 ins emacs  # Duh...
 ins htop  # Top
 ins vtop  # Top
 ins gotop-cjbassi  # Nice top
+ins fonts-hack-otf  # Hack font
+ins fonts-hack-ttf  # Hack font
 
 # .bashrc
 cat .bashrc >> ~/.bashrc
@@ -28,6 +29,6 @@ cp .tmux.conf ~/.
 
 # Manually setup/install:
 # arc dark
-# Papirus fonts
+# Papirus icons
 # Upgrade python (3.6+)
 
