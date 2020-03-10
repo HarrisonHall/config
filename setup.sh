d27e908ead9e46d08175b2b57a7a12e93461996d
HarrisonHall <harrisonchristianhall@gmail.com>
2020-03-10 08:08:13 -0400

    suckless

--- setup.sh
+++ setup.sh
@@ -21,7 +21,12 @@ sudo apt-get update
 sudo apt-get install papirus-icon-theme
 
 # Ubuntu
-ins unity-tweak-tool
+#ins unity-tweak-tool
+
+# Suckless
+mkdir ~/Documents/programs
+git clone https://git.suckless.org/st ~/Documents/programs/st
+git clone https://git.suckless.org/dwm ~/Documents/programs/dwm
 
 # Setup config
 cp -r .emacs.d ~/.
