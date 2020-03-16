e7efc9c8db78a0ea62814771bb1d462b856b64bb
HarrisonHall <harrisonchristianhall@gmail.com>
2020-03-16 11:23:38 -0400

    added kazam

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-choices="screenshot\nexit"
+choices="screenshot\nscreenrecording\nexit"
 choice=$( echo -e $choices | ~/Documents/.tmuxrc/dmenu/dmenu)
 
 case $choice in
     "screenshot")
         scrot -s /tmp/tmp.png && xclip -selection clipboard -t image/png -i /tmp/tmp.png && rm /tmp/tmp.png
+        ;;
+    "screenrecording")
+        kazam
         ;;
     *)
 esac
