5b43d2e11b2bc4cf1ca5089a5b494a4bb46eb325
harrisonhall <harrisonchristianhall@gmail.com>
2020-05-10 14:08:07 -0400

    lua

--- micro/emacs-ctrl-l/ctrl-l.lua
+++ micro/emacs-ctrl-l/ctrl-l.lua
@@ -1,9 +1,18 @@
 VERSION = "1.0.0"
 local micro = import("micro")
 
-just_pressed = false
 
+function init()
+	just_pressed = -1
+	return true
+end
 
+function onBufferOpen(buf)
+	return true
+end
 
 function onCenter(bufpane)
+	just_pressed = (just_pressed + 1) % 3
+	local v = bufpane.GetView()
+	return true
 end
