61032b88d237c023d894ccd582326d38f339fbf6
Harrison Hall <harrisonchristianhall@gmail.com>
2020-07-08 21:32:42 -0400

    Moving mod key from alt to super

--- apps/dwm/config.h
+++ apps/dwm/config.h
@@ -54,7 +54,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      comboview,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
