5d60a995eca82da81433488bc91a67a67794cb11
Harrison Hall <harrisonchristianhall@gmail.com>
2020-08-31 11:01:48 -0400

    dwm ipc mode (for polybar), faster resize refresh rate (60->1000)

--- apps/dwm/config.h
+++ apps/dwm/config.h
@@ -25,7 +25,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "α", "β", "γ", "δ", "ε", "ζ", "η", "θ"}; // ⚃ ●
+static const char *tags[] = { "●", "●", "●", "●", "●", "●", "●", "●"}; // ⚃ ●
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -42,9 +42,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.5;  // factor of master area size [0.05..0.95]
-static const int nmaster     = 1;    // number of clients in master area
-static const int resizehints = 0;    // 1 means respect size hints in tiled resizals  // used to be 1
+static const float mfact     = 0.5; //.5025 /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */  // used to be 1
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -124,6 +124,25 @@ static Key keys[] = {
   TAGKEYS(                        XK_9,                      8)
   /*{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },  // close dwm */
   { MODKEY|ControlMask,           XK_q,      quit,           {1} },  // restart dwm (keep apps open)
+};
+
+static const char *ipcsockpath = "/tmp/dwm.sock";
+static IPCCommand ipccommands[] = {
+  IPCCOMMAND(  view,                1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  toggleview,          1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  tag,                 1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  toggletag,           1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  tagmon,              1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  focusmon,            1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  focusstack,          1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  zoom,                1,      {ARG_TYPE_NONE}   ),
+  IPCCOMMAND(  spawn,               1,      {ARG_TYPE_PTR}    ),
+  IPCCOMMAND(  incnmaster,          1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  killclient,          1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  togglefloating,      1,      {ARG_TYPE_NONE}   ),
+  IPCCOMMAND(  setmfact,            1,      {ARG_TYPE_FLOAT}  ),
+  IPCCOMMAND(  setlayoutsafe,       1,      {ARG_TYPE_PTR}    ),
+  IPCCOMMAND(  quit,                1,      {ARG_TYPE_NONE}   )
 };
 
 /* button definitions */
